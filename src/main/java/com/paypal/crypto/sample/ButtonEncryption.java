379a902b9dbe96a9f030b3adbfdb78eb7048ba58
Mike Slinn <mslinn@micronauticsresearch.com>
2013-07-03 10:50:23 -0700

    Adapted help to sbt

--- src/main/java/com/paypal/crypto/sample/ButtonEncryption.java
+++ src/main/java/com/paypal/crypto/sample/ButtonEncryption.java
@@ -19,14 +19,15 @@ public class ButtonEncryption {
         Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
         if (args.length != 6 && args.length != 7) {
-            System.out.println("Usage: java ButtonEncryption <CertFile> <PrivKeyFile> <PPCertFile> <Password> <CmdTxt> <OutputFile> [sandbox]");
+            System.out.println("Usage: sbt 'run-main ButtonEncryption <CertFile> <PrivKeyFile> <PPCertFile> <Password> <CmdTxt> <OutputFile> [sandbox]'");
             System.out.println("  CertFile:    Your Public Cert");
             System.out.println("  PKCS12File:  Your Private Key in PKCS12 format");
             System.out.println("  PPCertFile:  PayPal's Public Cert");
             System.out.println("  KeyPassword: Password to sign with the private Key");
             System.out.println("  CmdTxt:      The button command, eg: 'cmd=_xclick,business=...'");
             System.out.println("  OutputFile:  File where the html will get written");
-            System.out.println("  Sandbox:     Optional. Put 'sandbox' here to test on sandbox accounts, or leave blank for live testing.");
+            System.out.println("  Sandbox:     Optional. Put 'sandbox' here to test on sandbox accounts, or leave blank for live testing.\n");
+            System.out.println("For example:\n  sbt 'run-main ButtonEncryption my-pubcert.pem my-prvkey.p12 paypal_pubcert.pem password cmd=_xclick,business=x@test.com,amount=23.00,currency_code=USD,item_name=todos son asi,cert_id=ZWTMUVWGGH8T8\" java_bn.html'");
             return;
         }
 
